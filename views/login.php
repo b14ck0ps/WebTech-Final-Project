bcb54c9fdb3c45708c823d9362b562d6bd5ebcda
Azran Hossain <mr69751@gmail.com>
2022-03-23 21:51:51 +0600

    link updated + beautify

--- views/login.php
+++ views/login.php
@@ -1,31 +1,30 @@
 <!DOCTYPE html>
+<html lang="en">
 <html>
-
 <head>
     <title> Portal </title>
     <link rel="stylesheet" href="../css/style.css">
-    <script src="../javascript/validation.js" defer></script>
+    <script src="../javascript/login-validation.js" defer></script>
 </head>
 
 <body>
-
-    <div class="container">
+    <div class="login-container">
         <form class="login-form" id="login" action="../controllers/LoginCheck.php" method="post">
             <div class="heading-container">
                 <h2>SCHOOL PORTAL</h2>
                 <p>Sign in with your organizational username</p>
-                <?php 
-                    if(isset($_GET['error'])) {
-                        echo '<p class="error">Wrong username or password</p>';
-                    }
+                <?php
+                if (isset($_GET['error'])) {
+                    echo '<p class="error">Wrong username or password</p>';
+                }
                 ?>
             </div>
             <div class="group login-input">
-                <input type="username" name="username" placeholder="User name" >
+                <input type="username" name="username" placeholder="User name">
                 <span id="username-error">The User Name field is required.</span>
             </div>
             <div class="group login-input">
-                <input type="password" name="password" placeholder="Password" >
+                <input type="password" name="password" placeholder="Password">
                 <span id="password-error">The password field is required.</span>
             </div>
             <button class="loginBtn" type="submit" name="submit">Log in</button>
