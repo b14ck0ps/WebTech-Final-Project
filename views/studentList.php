8970cb9d68ff01c92ff2f9f13adf74c02b46d8ed
Azran Hossain <mr69751@gmail.com>
2022-03-24 00:06:10 +0600

    organized

---
+++ views/studentList.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<body>
+    <div class="student-table">
+        <table>
+            <!-- will update form db in future  -->
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Status</th>
+            </tr>
+            <tr>
+                <td><a href="">19-39334-1</a></td>
+                <td>Rayops</td>
+                <td>ray@hotmail.com</td>
+                <td>Active</td>
+            </tr>
+            <tr>
+                <td><a href="">19-39334-1</a></td>
+                <td>Rayops</td>
+                <td>ray@hotmail.com</td>
+                <td>Active</td>
+            </tr>
+            <tr>
+                <td><a href="">19-39334-1</a></td>
+                <td>Rayops</td>
+                <td>ray@hotmail.com</td>
+                <td>Active</td>
+            </tr>
+            <tr>
+                <td><a href="">19-39334-1</a></td>
+                <td>Rayops</td>
+                <td>ray@hotmail.com</td>
+                <td>Active</td>
+            </tr>
+            <tr>
+                <td><a href="">19-39334-1</a></td>
+                <td>Rayops</td>
+                <td>ray@hotmail.com</td>
+                <td>Active</td>
+            </tr>
+        </table>
+    </div>
+</body>
+
+</html>
