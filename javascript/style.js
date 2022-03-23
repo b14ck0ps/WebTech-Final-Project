be6c2b9bd0d03fb97bc8cf4039a80d8fbca11155
Azran Hossain <mr69751@gmail.com>
2022-03-24 00:06:01 +0600

    small screen fix

--- javascript/style.js
+++ javascript/style.js
@@ -1,3 +1,4 @@
 function showItems() {
     document.getElementById("dropdown").classList.toggle("show");
 }
+document.getElementById("smallScreen").innerHTML = ("<h1><strong> SCREEN IS TOO SMALL <br> MOBILE VERSION IS NOT SUPPORTED </strong><h1>");
