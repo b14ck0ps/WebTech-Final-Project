ac1d547967afacb0b05f27ed0e9d9dee65578f5a
Azran Hossain <mr69751@gmail.com>
2022-03-24 23:56:07 +0600

    reg as admin 😆

--- views/registration.php
+++ views/registration.php
@@ -1,24 +1,38 @@
+<?php
+require_once('../controllers/pageAccess.php');
+?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
     <title>Registration</title>
 </head>
 
 <body>
-    <?php
-        if(isset($_GET['userType']) && $_GET['userType'] == 'student'){
-            require_once('r_student.html');
-            if(isset($_GET['msg']) && $_GET['msg'] == 'error'){
-                echo "Reg Error";
-            }
-        }else{
-            echo 'errror';
-        }
-    ?>
+    <div id="smallScreen"></div>
+    <div class="desktop">
+        <div class="container">
+            <?php include_once('navbar.php') ?>
+        </div>
+        <div class="main">
+            <?php include_once('sideNavbar.html') ?>
+            <div class="main-body">
+                <?php
+                if (isset($_GET['userType']) && $_GET['userType'] == 'admin') {
+                    require_once('r_admin.php');
+                    if (isset($_GET['msg']) && $_GET['msg'] == 'error') {
+                        echo "Reg Error";
+                    }
+                } else {
+                    echo '<center><h2 style="color: red"> PAGE NEED TO BE CONSTRUCT </h2></center>';
+                }
+                ?>
+            </div>
+        </div>
+    </div>
+    </div>
+    <script src="../javascript/style.js"></script>
 </body>
 
 </html>
