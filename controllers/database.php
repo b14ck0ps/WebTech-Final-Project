05296b3c96b00a0888dfeaf292de89ddd98377a4
Azran Hossain <mr69751@gmail.com>
2022-03-24 23:47:54 +0600

    database changed

--- controllers/database.php
+++ controllers/database.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost" , "root" , "" , "testdb");
+$conn = mysqli_connect("localhost" , "root" , "" , "portal");
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
