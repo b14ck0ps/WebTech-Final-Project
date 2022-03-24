95c393428efc4a8d90d9ca7438ad18354818cd73
Azran Hossain <mr69751@gmail.com>
2022-03-24 23:49:31 +0600

    post assoc index updated | admin reg page added

--- controllers/RegistrationCheck.php
+++ controllers/RegistrationCheck.php
@@ -1,19 +1,20 @@
 <?php
 if (
-    $_POST['username'] != null && $_POST['password'] != null && $_POST['first_name'] != null && $_POST['last_name'] != null
-    && $_POST['gender'] && $_POST['email'] && $_POST['phone']
+    $_POST['f_name'] != null && $_POST['l_name'] != null && $_POST['gender'] != null && $_POST['username'] != null
+    && $_POST['password'] != null && $_POST['email'] != null && $_POST['phone'] != null &&  $_POST['address'] != null
 ) {
     require_once('../model/adminModel.php');
     registration(
-        $_POST['id'],
+        $_POST['f_name'],
+        $_POST['l_name'],
+        $_POST['gender'],
         $_POST['username'],
         $_POST['password'],
-        $_POST['first_name'],
-        $_POST['last_name'],
-        $_POST['gender'],
         $_POST['email'],
-        $_POST['phone']
+        $_POST['phone'],
+        $_POST['address']
     );
 } else {
     echo "null submission";
 }
+// $f_name, $l_name, $gender,$username,$password,$email,$phone,$address
