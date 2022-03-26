50d5dfae0528d90dcbe8666a8b6f3c1e87767aa6
Azran Hossain <mr69751@gmail.com>
2022-03-26 17:06:05 +0600

    upload style, edit style and more added

--- javascript/style.js
+++ javascript/style.js
@@ -1,4 +1,11 @@
 function showItems() {
     document.getElementById("dropdown").classList.toggle("show");
 }
+
+function replaceEmptySrc(image) {
+    if (image.complete && image.naturalHeight == 0) {
+        image.src = '../assets/default/profile_picture.jpg';
+    }
+}
 document.getElementById("smallScreen").innerHTML = ("<h1><strong> SCREEN IS TOO SMALL <br> MOBILE VERSION IS NOT SUPPORTED </strong><h1>");
+replaceEmptySrc(document.getElementById("profile_picture"));
