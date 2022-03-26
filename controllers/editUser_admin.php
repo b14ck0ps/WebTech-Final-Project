b69c3013ce9a6c50b4638181d76cad8a43497f5a
Azran Hossain <mr69751@gmail.com>
2022-03-26 17:04:17 +0600

    admin can edit profiles

---
+++ controllers/editUser_admin.php
@@ -0,0 +1,44 @@
+<?php
+require_once('upload.php');
+if (
+    $_POST['f_name'] != null && $_POST['l_name'] != null &&  $_POST['username'] != null
+    && $_POST['email'] != null && $_POST['phone'] != null &&  $_POST['address'] != null
+) {
+    if (!isset($_POST['gender']))
+        $_POST['gender'] = 'N/A';
+    if (!isset($_POST['dob']))
+        $_POST['dob'] = null;
+    if (isset($_FILES['profile_picture']) && $_FILES['profile_picture']['name'] != '') {
+        if (upload($_FILES['profile_picture'], '../assets/admins/', $_POST['username'])) {
+            $upload = true;
+            $profile_pic_link = '../assets/admins/' . $_POST['username'] . '.jpg';
+        } else {
+            $upload = false;
+            $profile_pic_link = $_POST['pre_profile_picture'];
+            //upload fail so use default place holder
+        }
+    } else {
+        $upload = true;
+        $profile_pic_link = $_POST['pre_profile_picture'];
+    }
+    require_once('../model/adminModel.php');
+    if (userUpdate(
+        $_POST['f_name'],
+        $_POST['l_name'],
+        $_POST['gender'],
+        $_POST['dob'],
+        $_POST['username'],
+        $_POST['email'],
+        $_POST['phone'],
+        $_POST['address'],
+        $profile_pic_link
+    )) {
+        if ($upload)
+            $msg = 'success';
+        else
+            $msg = 'uploadfail';
+        header("location: ../views/profile.php?username=" . $_POST['username'] . "&&msg={$msg}");
+    }
+} else {
+    echo "null submission";
+}
