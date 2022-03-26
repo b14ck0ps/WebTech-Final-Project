11fc81863d2470fe5029aeeef4d5b37faacc3349
Azran Hossain <mr69751@gmail.com>
2022-03-26 17:04:58 +0600

    file upload method implemented

--- controllers/RegistrationCheck.php
+++ controllers/RegistrationCheck.php
@@ -1,18 +1,36 @@
 <?php
+require_once('upload.php');
 if (
-    $_POST['f_name'] != null && $_POST['l_name'] != null && $_POST['gender'] != null && $_POST['username'] != null
+    $_POST['f_name'] != null && $_POST['l_name'] != null &&  $_POST['username'] != null
     && $_POST['password'] != null && $_POST['email'] != null && $_POST['phone'] != null &&  $_POST['address'] != null
 ) {
+    if(!isset($_POST['gender']))
+        $_POST['gender'] = 'N/A';
+    if(!isset($_POST['gender']))
+        $_POST['dob'] = 'N/A';
+    if(!isset($_FILES['profile_picture']) || $_FILES['profile_picture']['name'] == '')
+        $profile_pic_link = '../assets/default/profile_picture.jpg';
+    else{
+       if(upload($_FILES['profile_picture'], '../assets/admins/', $_POST['username'])){
+           $profile_pic_link = '../assets/admins/'.$_POST['username'].'.jpg';
+       }
+       else{
+           $profile_pic_link = '../assets/default/profile_picture.jpg';
+           //upload fail so use default place holder
+       }
+    }
     require_once('../model/adminModel.php');
     registration(
         $_POST['f_name'],
         $_POST['l_name'],
         $_POST['gender'],
+        $_POST['dob'],
         $_POST['username'],
         $_POST['password'],
         $_POST['email'],
         $_POST['phone'],
-        $_POST['address']
+        $_POST['address'],
+        $profile_pic_link
     );
 } else {
     echo "null submission";
