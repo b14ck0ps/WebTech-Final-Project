72e98f8d4e41291c0ed5417b1ea4ae3147e7f2a0
Azran Hossain <mr69751@gmail.com>
2022-03-26 19:43:07 +0600

    single login page works for all users

--- controllers/LoginCheck.php
+++ controllers/LoginCheck.php
@@ -2,13 +2,21 @@
 if (isset($_POST['username']) && isset($_POST['password'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
-    require_once('../model/adminModel.php');
+    require_once('../model/defaultModel.php');
     if (login($username, $password)) {
         session_start();
         $_SESSION['status'] = true;
         $_SESSION['username'] = $username;
-        // header('location: ../views/profile.php');
-        header('location: ../views/adminDashboard.php');
+        if (userinfo($username)['userType'] == 'admin')
+            header('location: ../views/adminDashboard.php');
+        else if (userinfo($username)['userType'] == 'stuff')
+            header('location: ../views/stuffDashboard.php');
+        else if (userinfo($username)['userType'] == 'faculty')
+            header('location: ../views/facultyDashboard.php');
+        else if (userinfo($username)['userType'] == 'student')
+            header('location: ../views/studentDashboard.php');
+        else
+            echo "Error";
     } else
         header('location: ../views/login.php?msg=error');
 } else
--- model/adminModel.php
+++ model/adminModel.php
@@ -1,17 +1,5 @@
 <?php
 require_once('../controllers/database.php');
-function login($username, $password)
-{
-    global $conn;
-    $sql = "SELECT * FROM admin_info WHERE username = '{$username}' AND password = '{$password}'";
-    $result = mysqli_query($conn, $sql);
-    $row = mysqli_fetch_assoc($result);
-    if ($row) {
-        return true;
-    } else {
-        return false;
-    }
-}
 function userinfo($username)
 {
     global $conn;
