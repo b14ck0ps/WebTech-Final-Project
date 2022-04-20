cf41946fe46c8ca350167fa79a1f4e55724eac62
Azran Hossain <mr69751@gmail.com>
2022-04-20 19:45:01 +0600

    build: all of the faculty functionlity added
    This commit belongs to [MOHAMMAD ABIR]

--- model/courseModel.php
+++ model/courseModel.php
@@ -10,3 +10,25 @@ function getResInfo($id)
     }
     return $info;
 }
+
+function manageTest($examName, $examLink)
+{
+    global  $conn;
+    $sql = "INSERT INTO tests  (examName,examLink)  values ('{$examName}','{$examLink}')";
+    $test = mysqli_query($conn, $sql);
+    if ($test) {
+        return true;
+    } else
+        return false;
+}
+
+function marks($id, $courseName, $marks)
+{
+    global  $conn;
+    $sql = "INSERT INTO results  (id,courseName,marks)  values ('{$id}','{$courseName}','{$marks}')";
+    $test = mysqli_query($conn, $sql);
+    if ($test) {
+        return true;
+    } else
+        return false;
+}
